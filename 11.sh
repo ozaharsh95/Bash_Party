8e5718198f3edc6dadd7db6726be68884cdd8d59
ozaharsh95 <dsouzaaron77@gmail.com>
2025-02-11 15:49:51 +0530

    to clean repo from local

--- 11.sh
+++ 11.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+read -p "Enter your number: " num
+
+if [[ $num -gt 0 ]] ; then
+	echo "The Number is positive"
+elif [[ $num -lt 0 ]] ; then
+	echo "The Number is negative"
+else
+	echo "The Number is 0"
+fi
---
+++ practice.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name="Bhavini"
+age=12
+
+echo "Hello $name, My age is $age"
+echo "$1 $2 $3"
